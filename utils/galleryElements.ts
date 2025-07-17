9f5e4857510a63a9ff72d5c555d4fda18b6a4fa2
Mohamad Wakkaf <107024291+Mohamad16593@users.noreply.github.com>
2025-07-17 16:23:09 +0300

    Update galleryElements.ts

--- utils/galleryElements.ts
+++ utils/galleryElements.ts
@@ -1,13 +1,13 @@
-import img1 from "@/assets/images/projectsImags/9.jpg";
-import img2 from "@/assets/images/projectsImags/24.jpg";
-import img3 from "@/assets/images/projectsImags/12.jpg";
-import img4 from "@/assets/images/projectsImags/16.jpg";
-import img5 from "@/assets/images/projectsImags/10.jpg";
-import img6 from "@/assets/images/projectsImags/18.jpg";
-import img7 from "@/assets/images/projectsImags/26.jpg";
-import img8 from "@/assets/images/projectsImags/43.jpg";
-import img9 from "@/assets/images/projectsImags/44.jpg";
-import img10 from "@/assets/images/projectsImags/47.jpg";
+import img1 from "@/assets/images/projectsImags/9.JPG";
+import img2 from "@/assets/images/projectsImags/24.JPG";
+import img3 from "@/assets/images/projectsImags/12.JPG";
+import img4 from "@/assets/images/projectsImags/16.JPG";
+import img5 from "@/assets/images/projectsImags/10.JPG";
+import img6 from "@/assets/images/projectsImags/18.JPG";
+import img7 from "@/assets/images/projectsImags/26.JPG";
+import img8 from "@/assets/images/projectsImags/43.JPG";
+import img9 from "@/assets/images/projectsImags/44.JPG";
+import img10 from "@/assets/images/projectsImags/47.JPG";
 
 export const swiperImages = [
   img1,
